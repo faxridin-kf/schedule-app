871ad74f254c50c003b124328aa544d990103e4d
fakhridin <faxridinkf@gmail.com>
2022-12-20 22:25:04 +0500

    feat: add all functions

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './App.css';
+import MonthlyCalendar from "./month/MonthlyCalendar";
+
+class App extends Component {
+  render() {
+    return (
+        <MonthlyCalendar/>
+    );
+  }
+}
+
+export default App;
